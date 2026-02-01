7a26d299265f831844df1fa027150c2cc006319c
Droid7770 <salkovkonstantin711@gmail.com>
2026-02-01 20:38:05 +0300

    Fixed save keybind bug and reverted optimizations

--- lua/custom/plugins/guess-indent.lua
+++ lua/custom/plugins/guess-indent.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return { 'NMAC427/guess-indent.nvim' }
+return {
+  'NMAC427/guess-indent.nvim',
+  opts = {
+    auto_cmd = true,
+  },
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,7 +5,8 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
-vim.keymap.set({ 'n', 'i' }, '<C-s>', ':wall<CR>', { desc = 'Write to all buffers' })
+vim.keymap.set({ 'n' }, '<C-s>', ':wall<CR>', { desc = 'Write to all buffers' })
+vim.keymap.set({ 'i' }, '<C-s>', '<C-c>:wall<CR>', { desc = 'Write to all buffers' })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
