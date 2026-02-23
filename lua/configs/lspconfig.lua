581c8c45344d25a0adc46d97dc131c7535dac358
fluqual <salkovkonstantin711@gmail.com>
2026-02-23 18:17:46 +0300

    Fixed LSP configs

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,28 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "rust_analyzer", "clangd", "ts_ls" }
+vim.lsp.config.ts_ls = {
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
+
+}
+vim.lsp.config.clangd = {
+          cmd = {
+            'clangd',
+            '--clang-tidy',
+            '--background-index',
+            '--offset-encoding=utf-8',
+          },
+          root_markers = { '.clangd', 'compile_commands.json' },
+          filetypes = { 'c', 'cpp' },
+}
+vim.lsp.config.rust_analyzer = {
+            cargo = {
+                allFeatures = true,
+                features = "all", -- alternative way to specify features
+            },
+          filetypes = { 'rs' },
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
